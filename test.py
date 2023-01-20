8d78b100be71a36a487e46aa1d5f5a03883fd289
Alex Kardash <alexandr.kardash@coinspaid.com>
2023-01-20 14:37:09 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.wikipedia.org/')
+
+driver.find_element(By.ID, 'searchInput').send_keys('Test automation')
+driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+
+assert 'Test automation' in driver.page_source
